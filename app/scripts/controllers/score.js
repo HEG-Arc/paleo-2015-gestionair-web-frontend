31fa1e4be051288eef34abb8ffd303d3c9d82854
Boris Fritscher <boris@fritscher.ch>
2015-07-15 21:58:18 +0200

    scoreboard test

--- app/scripts/controllers/score.js
+++ app/scripts/controllers/score.js
@@ -9,5 +9,7 @@
  */
 angular.module('gestionairApp')
   .controller('ScoreCtrl', function () {
+    //need question_id and lang lookup
+    this.scores = JSON.parse('[{"name":"Alice","score":3,"languages":[{"lang":"vn","correct":1},{"lang":"vn","correct":0},{"lang":"de","correct":1},{"lang":"ku","correct":1}]},{"name":"Charles","score":2,"languages":[{"lang":"ku","correct":0},{"lang":"vn","correct":1},{"lang":"ro","correct":1},{"lang":"ku","correct":0}]},{"name":"Felicitas","score":1,"languages":[{"lang":"es","correct":0},{"lang":"vn","correct":1},{"lang":"ru","correct":0},{"lang":"ru","correct":0}]},{"name":"Bertrand","score":0,"languages":[{"lang":"es","correct":0},{"lang":"ku","correct":0},{"lang":"ku","correct":0},{"lang":"ch","correct":0}]},{"name":"Delphine","score":0,"languages":[{"lang":"es","correct":0},{"lang":"hu","correct":0},{"lang":"es","correct":0},{"lang":"vn","correct":0}]},{"name":"Elisabeth","score":0,"languages":[{"lang":"es","correct":0},{"lang":"ne","correct":0},{"lang":"es","correct":0}]}]');
 
   });
