7816739230a12c375080fadc81d50ab34ddbb97a
Boris Fritscher <boris@fritscher.ch>
2016-09-08 11:59:21 +0200

    better css for smaller screens

--- app/scripts/directives/slideshow.js
+++ app/scripts/directives/slideshow.js
@@ -9,7 +9,7 @@
 angular.module('gestionairApp')
   .directive('slideshow', function ($timeout) {
     return {
-      template: '<div class="slideshow"flex><img ng-src="{{images[activeIndex]}}"/><div class="fade" layout-fill ng-repeat="src in images" ng-show="$index == activeIndex" style="background-image: url(\'{{src}}\')"></div></div>',
+      template: '<div class="slideshow"flex><img style="width:100%" ng-src="{{images[activeIndex]}}"/><div class="fade" layout-fill ng-repeat="src in images" ng-show="$index == activeIndex" style="background-image: url(\'{{src}}\')"></div></div>',
       replace: true,
       restrict: 'E',
       scope: {
